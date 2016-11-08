b62a081eeb768eff5c0fd72586f1f4f04ad0969a
lelea2 <daomaikhanh1988@yahoo.com>
2016-11-08 10:19:36 -0800

    Initial check in

---
+++ assigment1/assigment1/View2Controller.swift
@@ -0,0 +1,23 @@
+//
+//  View2Controller.swift
+//  assigment1
+//
+//  Created by Dao, Khanh on 11/8/16.
+//  Copyright © 2016 cmpe297. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class View2Controller : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+}
---
+++ assigment1/assigment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assigment1
+//
+//  Created by Dao, Khanh on 11/8/16.
+//  Copyright © 2016 cmpe297. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
