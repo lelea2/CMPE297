9a80ad803ebd90a3a3ae3d55ca68efd7358e5d98
lelea2 <daomaikhanh1988@yahoo.com>
2016-11-12 15:27:56 -0800

    Initial check in

--- assignment2/assignment2/ViewController.swift
+++ assignment2/assignment2/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var studentIdField: UITextField!
+
+    @IBOutlet var groupField: UITextField!
+    
+    @IBOutlet var bloodTypeField: UITextField!
+    
+    @IBOutlet var medicationField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func saveArchiveBtn(sender: UIButton) {
+    }
+    
+    
+    @IBAction func openArchiveBtn(sender: UIButton) {
+    }
+    
 }
 
