46185a8ee9d88d4fdf76cac3d28b7b7ff5e1ba68
lelea2 <daomaikhanh1988@yahoo.com>
2016-11-14 07:32:26 -0800

    Initial check in

--- assignment2/assignment2/ViewController.swift
+++ assignment2/assignment2/ViewController.swift
@@ -18,21 +18,49 @@ class ViewController: UIViewController {
     
     @IBOutlet var medicationField: UITextView!
     
+    var uniqueFileName = "assigment2.archive"
+    var dataFilePath: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Load file check
+//        dataFilePath = fileInDocumentsDirectory(uniqueFileName)
+//        print(dataFilePath)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    @IBAction func saveArchiveBtn(sender: UIButton) {
+    
+    
+    //Helper function to get directory URL
+    func documentsDirectory() -> String {
+        let documentsFolderPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0]
+        return documentsFolderPath
+    }
+    //Helper function to get filepath
+    func fileInDocumentsDirectory(filename: String) -> String {
+        let writePath = (documentsDirectory() as NSString).stringByAppendingPathComponent("Mobile")
+        //Check file if existing
+        if (!NSFileManager.defaultManager().fileExistsAtPath(writePath)) {
+            do {
+                try NSFileManager.defaultManager().createDirectoryAtPath(writePath, withIntermediateDirectories: false, attributes: nil) }
+            catch let error as NSError {
+                print(error.localizedDescription);
+            }
+        }
+        return (writePath as NSString).stringByAppendingPathComponent(filename)
     }
     
+
+    @IBAction func saveArchiveBtn(sender: UIButton) {
+        let cardArr = [studentIdField.text, bloodGroupField.text, allergiesField.text, medicationField.text]
+        NSKeyedArchiver.archiveRootObject(cardArr, toFile: dataFilePath!)
+    }
     
     @IBAction func openArchiveBtn(sender: UIButton) {
+        let dataArray = NSKeyedUnarchiver.unarchiveObjectWithFile(dataFilePath!) as! [String]
     }
     
 }
