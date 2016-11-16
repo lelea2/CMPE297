e95d66edf3d5328d02c33e3c8413cf144ebf58a6
lelea2 <daomaikhanh1988@yahoo.com>
2016-11-15 18:02:23 -0800

    Initial check in

--- assignment3/assignment3/ViewController.swift
+++ assignment3/assignment3/ViewController.swift
@@ -24,12 +24,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func generateString() -> String {
+        let str = bookNameField.text! + ", " + authorField.text! + ", " + descField.text!
+        return str
+    }
     
-
     @IBAction func saveStorage(sender: UIButton) {
+        // get the documents folder url
+        let documentDirectoryURL = try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
+        // create the destination url for the text file to be saved
+        let fileDestinationUrl = documentDirectoryURL.URLByAppendingPathComponent("Assigment3.txt")        
+        let text = generateString()
+        
+        do {
+            // writing to disk
+            try text.writeToURL(fileDestinationUrl, atomically: false, encoding: NSUTF8StringEncoding)
+            
+            // saving was successful. any code posterior code goes here
+            
+            // reading from disk
+            do {
+                let mytext = try String(contentsOfURL: fileDestinationUrl, encoding: NSUTF8StringEncoding)
+                print(mytext)   // "some text\n"
+            } catch let error as NSError {
+                print("error loading from url \(fileDestinationUrl)")
+                print(error.localizedDescription)
+            }
+        } catch let error as NSError {
+            print("error writing to url \(fileDestinationUrl)")
+            print(error.localizedDescription)
+        }
     }
 
     @IBAction func saveArchive(sender: UIButton) {
+        
     }
     
 }
