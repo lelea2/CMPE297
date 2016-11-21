523adbea1a29112ad2d9adb62063e4e31e2ddc41
lelea2 <daomaikhanh1988@yahoo.com>
2016-11-21 14:48:00 -0800

    Initial check in for sensor

---
+++ sensor/sensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sensor
+//
+//  Created by Dao, Khanh on 11/21/16.
+//  Copyright © 2016 cmpe297. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
