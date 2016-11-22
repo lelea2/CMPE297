e3d5f96f5fa7f8ff9eef41bd3d6027f6cf3741ff
lelea2 <daomaikhanh1988@yahoo.com>
2016-11-22 15:08:40 -0800

    Detect fall app

---
+++ falldetect/falldetect/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  falldetect
+//
+//  Created by Dao, Khanh on 11/22/16.
+//  Copyright © 2016 cmpe297. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func startBtn(sender: UIButton) {
+    }
+
+
+    @IBAction func stopBtn(sender: UIButton) {
+    }
+
+}
+
