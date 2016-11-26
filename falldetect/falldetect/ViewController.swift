b0e5febaab3f8525cc47b949aeb07ff305e34c97
Khanh Dao <kdao@Khanhs-MBP-2.attlocal.net>
2016-11-26 08:31:29 -0800

    Finish detect phone fall

--- falldetect/falldetect/ViewController.swift
+++ falldetect/falldetect/ViewController.swift
@@ -6,15 +6,59 @@
 //  Copyright © 2016 cmpe297. All rights reserved.
 //
 
+//Testing concept referenced from http://stackoverflow.com/questions/14445914/detect-programmatically-if-iphone-is-dropped-using-coremotion-accelerometer
+
 import UIKit
+import CoreMotion
+import Darwin
 
 class ViewController: UIViewController {
 
-    var shouldDetect = false;
+    @IBOutlet var detectText: UILabel!
+    
+    var motionManager = CMMotionManager()
+    var detected = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        motionManager.gyroUpdateInterval = 0.2
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.deviceMotionUpdateInterval = 0.1
+//        self.detectAccel()
+    }
+    
+//    func detectDeviceMotion() {
+//        motionManager.startDeviceMotionUpdates(to: OperationQueue(), withHandler: {
+//            (deviceMotion, error) -> Void in
+//            if(error == nil) {
+//                print(deviceMotion)
+//            } else {
+//                //handle the error
+//            }
+//        })
+//    }
+    
+    func detectAccel() {
+        self.detectText.text = "Detecting..."
+        motionManager.startAccelerometerUpdates(to: OperationQueue()) { (accelerometerData: CMAccelerometerData?, NSError) -> Void in
+            let acceleration = accelerometerData!.acceleration
+            let a = pow(acceleration.x, 2) + pow(acceleration.y, 2) + pow(acceleration.z, 2)
+            print(a)
+            //By theory, if a drop to 0, mean phone is dropping
+            DispatchQueue.main.async(execute: { () -> Void in
+                if (a < 0.1) {
+                    self.detectText.text = "Detected"
+                    print("Detect phone fall...")
+                    self.motionManager.stopAccelerometerUpdates()
+                } else {
+                    //phone not drop
+                }
+            })
+            if(NSError != nil) {
+                print("\(NSError)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,12 +68,14 @@ class ViewController: UIViewController {
 
 
     @IBAction func startBtn(_ sender: UIButton) {
-        shouldDetect = true
+        self.detectAccel()
     }
 
 
     @IBAction func stopBtn(_ sender: UIButton) {
-        shouldDetect = false
+//        detected = false
+        detectText.text = "Not Detected"
+        motionManager.stopAccelerometerUpdates() //Stop update
     }
 
 }
