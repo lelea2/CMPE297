f8980abc5fbd105750710c8a38bea70d4ce13578
lelea2 <daomaikhanh1988@yahoo.com>
2016-12-05 14:51:11 -0800

    Initial check in'

---
+++ self_assistant/self_assistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  self_assistant
+//
+//  Created by Dao, Khanh on 12/5/16.
+//  Copyright © 2016 cmpe297. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
