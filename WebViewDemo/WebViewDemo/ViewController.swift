97222e138086330930b22ad534bbc913a8d481d8
khanh.dao <khanh.dao@move.com>
2017-03-10 18:52:11 -0800

    Clean up

---
+++ WebViewDemo/WebViewDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebViewDemo
+//
+//  Created by Dao, Khanh on 2/12/17.
+//  Copyright © 2017 kdao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
